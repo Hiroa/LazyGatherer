cd46c14a433f03ddd0c52dded189f02148f2c9a2
Hiroa <hirroa@gmail.com>
2025-11-04 19:09:44 +0100

    Fix loading error

--- LazyGatherer/Models/ComparerEnum.cs
+++ LazyGatherer/Models/ComparerEnum.cs
@@ -1,7 +1,12 @@
-﻿namespace LazyGatherer.Models;
+﻿using System.Runtime.Serialization;
+
+namespace LazyGatherer.Models;
 
 public enum ComparerEnum
 {
+    [EnumMember(Value = "Max yield")]
     MaxYield,
+
+    [EnumMember(Value = "Max Yield per GP")]
     MaxYieldPerGp
 }
